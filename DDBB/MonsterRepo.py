bab66f16d34d39269f43e616c8fdf0305c599b60
MarioEros <erosrodespi@gmail.com>
2022-06-11 21:35:27 +0200

    Monsters updated

--- DDBB/MonsterRepo.py
+++ DDBB/MonsterRepo.py
@@ -1,4 +1,6 @@
 import sqlite3
+
+import clases
 from DDBB.mensajes import get_message
 
 
@@ -7,21 +9,28 @@ con = sqlite3.connect('../Datos/mhw.db')
 cursor = con.cursor()
 
 '''
-('monster',)('monster_text',)
-('monster_hitzone',)('monster_hitzone_text',)('monster_break',)('monster_break_text',)
-('monster_habitat',)('monster_reward',)('monster_reward_condition_text',)
+db_table_content('monster_base') #['MHCOMPI_ID', 'name_en', 'ecology_en', 'size', 'traps']
+db_table_content('monster_weaknesses') #['MHCOMPI_ID', 'name_en', 'form', 'alt_description',
+                                          'fire', 'water', 'thunder', 'ice', 'dragon',
+                                          'poison', 'sleep', 'paralysis', 'blast', 'stun']
+db_table_content('monster_habitats') #['MHCOMPI_ID', 'name_en', 'map_en', 'start_area']
 '''
 def get_monster_by_lang(mons:str, lang:str):
-    cursor.execute("SELECT * FROM monster_text WHERE name Like ? AND lang_id = ? COLLATE NOCASE",('%'+mons+'%',lang))
+    cursor.execute('''
+    SELECT * FROM
+    monster_weaknesses b
+    WHERE b.name_en = ? COLLATE NOCASE''',(mons,))
     print([x[0] for x in cursor.description])
     mons = [x for x in cursor]
     if len(mons) == 0:
         print(get_message('no_encontrado',lang))
     elif len(mons) == 1:
         print(mons[0])
+        monstruo = clases.Monstruo(mons[0])
+        print(monstruo)
     else:
         print(get_message('varios_encontrados',lang))
-        [print(x[2]) for x in mons]
+        [print(x) for x in mons]
 
 
 
@@ -31,15 +40,24 @@ def db_table_content(table: str):
     for row in cursor:
         print(row)
 
+
+def db_table_content_where(table: str, mons: str):
+    rows = cursor.execute("SELECT * FROM {0} where name_en LIKE '%{1}%' COLLATE NOCASE;".format(table,mons))
+    print([x[0] for x in cursor.description])
+    for row in cursor:
+        print(row)
+
+
 def get_hey():
     rows = cursor.execute("SELECT * FROM monster_text WHERE alt_state_description IS NOT NULL ;")
     print([x[0] for x in cursor.description])
     for row in cursor:
         print(row)
 
-db_table_content('monster')
+# db_table_content('monster')
 # db_table_content('monster_text')
-# get_monster_by_lang("Alatre","es")
+# db_table_content_where('monster_habitats','pukei')
+get_monster_by_lang("Pukei-Pukei","es")
 # get_hey()
 # db_table_content('monster_hitzone')
 # db_table_content('monster_hitzone_text')
--- DDBB/clases.py
+++ DDBB/clases.py
@@ -1,21 +1,34 @@
 class Monstruo:
-  nombre = ''
-  desc = ''
-  url = ''
-  debil = []
-  estado=''
-  rompible = ''
+  def __init__(self,monster):
+    self.nombre = monster[1]
+    self.ecology = monster[2]
+    self.size = monster[3]
+    self.traps = monster[4]
+
+  def __str__(self):
+    return f'Monster({self.nombre,self.ecology,self.size,self.traps})'
+
 
 class Debilidad:
-  forma = ''
-  descripcion = ''
-  elemento=''
-  estado=''
-  def __init__(self,forma,descripcion,elemento,estado):
-    self.forma=forma
-    self.descripcion=descripcion
-    self.elemento=elemento
-    self.estado=estado
+  def __init__(self,monster):
+    self.form = monster[2]
+    self.alt_description = monster[3]
+    self.fire = monster[4]
+    self.water = monster[5]
+    self.thunder = monster[6]
+    self.ice = monster[7]
+    self.dragon = monster[8]
+    self.poison = monster[9]
+    self.sleep = monster[10]
+    self.paralysis = monster[11]
+    self.blast = monster[12]
+    self.stun = monster[13]
+
+  def __str__(self):
+    return f'''Monster({self.form,self.alt_description,
+                        self.fire,self.water,self.thunder,self.ice,self.dragon,
+                        self.poison,self.sleep,self.paralysis,self.blast,self.stun,})'''
+
 
 class Objeto:
   nombre = ''
