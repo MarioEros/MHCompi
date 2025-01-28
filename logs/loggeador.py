0ac0396f766aa1be0cc2a3916a10a8c8b75e3b89
MarioEros <erosrodespi@gmail.com>
2025-01-28 22:07:24 +0100

    Changed Paths to be OS insensitive

--- logs/loggeador.py
+++ logs/loggeador.py
@@ -4,12 +4,12 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def loggear(texto):
   print(texto)
-  with open(ROOT_DIR+'\\monscomp.log', 'a') as f:
+  with open(os.path.join(ROOT_DIR,'monscomp.log'), 'a') as f:
     f.write(texto)
     f.write('\n')
 
 def loggear_DB(texto):
   print(texto)
-  with open(ROOT_DIR+'\\DB.log', 'a') as f:
+  with open(os.path.join(ROOT_DIR,'DB.log'), 'a') as f:
     f.write(texto)
     f.write('\n')
